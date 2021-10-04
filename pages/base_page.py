3540e258426bf0bf398ceb3bbf9de50ff62f038f
Ion Motelica <ion.motelica@eu.rsystems.com>
2021-10-04 18:12:27 +0300

    remove unused functions

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -19,21 +18,6 @@ class BasePage(object):
 
     def find_elements(self, *locator):
         return self.driver.find_elements(*locator)
-
-    def open(self, url):
-        url = self.base_url + url
-        self.driver.get(url)
-
-    def get_title(self):
-        return self.driver.title
-
-    def get_url(self):
-        return self.driver.current_url
-
-    def hover(self, *locator):
-        element = self.find_element(*locator)
-        hover = ActionChains(self.driver).move_to_element(element)
-        hover.perform()
 
     def wait_element(self, *locator):
         try:
