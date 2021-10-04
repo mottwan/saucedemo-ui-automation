cc135b43cae8d10bfb47e3f717a446fbbc698ced
Ion Motelica <ion.motelica@eu.rsystems.com>
2021-10-04 16:35:59 +0300

    add base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+class BasePage(object):
+    def __init__(self, driver, base_url='https://www.saucedemo.com/'):
+        self.base_url = base_url
+        self.driver = driver
+        self.timeout = 10
+
+    def check_page_loaded(self, *locator):
+        self.wait_element(*locator)
+        return True if self.find_element(*locator) else False
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    def find_elements(self, *locator):
+        return self.driver.find_elements(*locator)
+
+    def open(self, url):
+        url = self.base_url + url
+        self.driver.get(url)
+
+    def get_title(self):
+        return self.driver.title
+
+    def get_url(self):
+        return self.driver.current_url
+
+    def hover(self, *locator):
+        element = self.find_element(*locator)
+        hover = ActionChains(self.driver).move_to_element(element)
+        hover.perform()
+
+    def wait_element(self, *locator):
+        try:
+            WebDriverWait(self.driver, self.timeout).until(EC.presence_of_element_located(locator))
+        except TimeoutException:
+            print("\n * ELEMENT NOT FOUND WITHIN GIVEN TIME! --> %s" % (locator[1]))
+            self.driver.quit()
