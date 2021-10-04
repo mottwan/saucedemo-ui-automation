c6ae4843a4e5018eacb7e3c7c046cac995789342
Ion Motelica <ion.motelica@eu.rsystems.com>
2021-10-04 16:42:25 +0300

    add products page object

---
+++ pages/products_page.py
@@ -0,0 +1,36 @@
+import sys
+# import os
+sys.path.append(sys.path[0] + "/...")
+# Uncomment if the above example gives you a relative path error
+# sys.path.append(os.getcwd())
+
+from pages.base_page import BasePage
+from utils.locators import Locators
+
+
+# Page objects are written in this module.
+# Depends on the page functionality we can have more functions for new classes
+
+class ProductsPage(BasePage):
+
+    def __init__(self, driver):
+        self.locator = Locators
+        super().__init__(driver)
+
+    def page_should_be_loaded(self):
+        return self.check_page_loaded(*self.locator.APPLICATION_LOGO)
+
+    def get_products_list(self):
+        self.find_elements(*self.locator.INVENTORY_LIST)
+        items = self.find_elements(*self.locator.INVENTORY_LIST)
+        return [{'name': item.find_element(*self.locator.ITEM_NAME).text,
+                 'price': item.find_element(*self.locator.ITEM_PRICE).text, 'quantity': 1} for item in items]
+
+    def add_to_shopping_cart(self, items: list):
+        products = self.find_elements(*self.locator.INVENTORY_LIST)
+        for product in products:
+            product_name = self.find_element(*self.locator.ITEM_NAME)
+            for item in items:
+                if item['name'] in product_name.text:
+                    product.find_element(*self.locator.ADD_TO_CART).click()
+        return items
