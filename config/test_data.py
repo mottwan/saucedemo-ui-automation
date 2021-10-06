07e375ae044e53737238d3893938ce31628aff90
Ion Motelica <ion.motelica@eu.rsystems.com>
2021-10-06 10:17:20 +0300

    renamed utils to config and moved files

--- config/test_data.py
+++ config/test_data.py
@@ -9,3 +9,14 @@ def get_executable_file_path_for(filename):
 
 def __executable_file_name(binary_file):
     return '{}.exe'.format(binary_file) if platform.system() == 'Windows' else binary_file
+
+
+class TestData:
+    CHROME_EXECUTABLE = get_executable_file_path_for("chromedriver")
+    FIREFOX_EXECUTABLE = get_executable_file_path_for("geckodriver")
+    BASE_URL = 'https://www.saucedemo.com/'
+    USERNAME = 'standard_user'
+    PASSWORD = 'secret_sauce'
+    FIRST_NAME = 'John'
+    LAST_NAME = 'Travolta'
+    ZIP_CODE = 'MD-2001'
--- config/webdriver.py
+++ config/webdriver.py
@@ -1,14 +1,5 @@
 from selenium import webdriver
-from .driverconf import get_executable_file_path_for
-
-
-class WebBrowser:
-    @staticmethod
-    def set_name(browser_name: str):
-        if browser_name.lower() in ['chrome', 'chromedriver', 'google-chrome', 'ch']:
-            return Chrome.set_driver()
-        elif browser_name.lower() in ['ff', 'firefox', 'gecko', 'geckodriver']:
-            return Firefox.set_driver()
+from .test_data import TestData
 
 
 class Chrome:
@@ -20,8 +11,7 @@ class Chrome:
         options.add_argument('disable-infobars')
         options.add_argument("--disable-extensions")
         options.add_argument('--disable-gpu')
-        return webdriver.Chrome(executable_path=get_executable_file_path_for('chromedriver'),
-                                options=options)
+        return webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE, options=options)
 
 
 class Firefox:
@@ -33,5 +23,4 @@ class Firefox:
         options.add_argument('disable-infobars')
         options.add_argument("--disable-extensions")
         options.add_argument('--disable-gpu')
-        return webdriver.Firefox(executable_path=get_executable_file_path_for('geckodriver'),
-                                 options=options)
+        return webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE, options=options)
