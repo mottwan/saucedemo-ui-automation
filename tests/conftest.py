b2ad657c151c0c6525665a97b60d29497f4a770f
Ion Motelica <ion.motelica@eu.rsystems.com>
2021-10-06 10:22:30 +0300

    remove conftest.py duplicate

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,14 @@ def init_driver(request):
     yield
     web_driver.close()
     web_driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    setattr(report, "duration_formatter", "%H:%M:%S.%f")
+
+
+def pytest_html_report_title(report):
+    report.title = "Swag Labs Python Test Automation!"
